d06ce01e122d5ca843de842aa739a2abcb521260
dalong <dalong0514@126.com>
2020-06-16 20:16:17 +0800

    start.js 例子

---
+++ spec/startSpec.js
@@ -0,0 +1,8 @@
+describe('Title of a Suite', function() {
+    let numberA = 3;
+    let numberB = 2;
+
+    it('Testing the add() function', function() {
+        expect(add(numberA, numberB)).toEqual(5);
+    });
+});

76ed7d8461135230aa65d0ce9657379bb8deff6b
dalong <dalong0514@126.com>
2020-06-16 21:26:02 +0800

    4.5 Set up and tear down

--- spec/startSpec.js
+++ spec/startSpec.js
@@ -2,32 +2,21 @@ describe('Setup and Teardown', () => {
     let count = 0;
     let velocity = 0;
     beforeEach(() => {
-        velocity = 100;
-        count++;
-        console.log('Count is ' + count);
+        this.velocity = 100;
     });
 
     afterEach(() => {
-        velocity = 0;
-        console.log('Some spec just finished and this function is called');
-    });
-
-    beforeAll(() => {
-        console.log('This is called only one, specs are about to run.');
-    });
-
-    afterAll(() => {
-        console.log('All specs finished, time for cleanup');
+        this.velocity = 0;
     });
 
     it('Testing Velocity and reducing velocity', () => {
-        expect(velocity).toEqual(100);
-        velocity = 20;
-        expect(velocity).toBe(20);
+        expect(this.velocity).toEqual(100);
+        this.velocity = 20;
+        expect(this.velocity).toBe(20);
+        this.acceleration = 5;
     })
 
     it('Testing Velocity', () => {
-        expect(velocity).toEqual(100);
-        expect(true).toEqual(true);
+        expect(this.acceleration).toBeUndefined();
     });
 });

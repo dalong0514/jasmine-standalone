aec420d4b35297536b0fef3500dc7a63157045a6
dalong <dalong0514@126.com>
2020-06-17 14:31:53 +0800

    4.6 Probing The Boundaries

--- spec/profitSpec.js
+++ spec/profitSpec.js
@@ -10,4 +10,39 @@ describe('province', function() {
     it('profit', function() {
         expect(asia.profit).toEqual(230);
     })
+    it('change production', function() {
+        asia.producers[0].production = 20;
+        expect(asia.shortfall).toEqual(-6);
+        expect(asia.profit).toEqual(292);
+    })
+    it('zero demand', function() {
+        asia.demand = 0;
+        expect(asia.shortfall).toEqual(-25);
+        expect(asia.profit).toEqual(0);
+    })
+    it('negative demand', function() {
+        asia.demand = -1;
+        expect(asia.shortfall).toEqual(-26);
+        expect(asia.profit).toEqual(-10);
+    })
 });
+
+describe('no producers', function() {
+    let noProducers;
+    beforeEach(function() {
+        const data = {
+            name: 'No producers',
+            producers: [],
+            demand: 30,
+            price: 20,
+        };
+        noProducers = new Province(data);
+        console.log(noProducers);
+    });
+    it('shortfall', function() {
+        expect(noProducers.shortfall).toEqual(30);
+    });
+    it('profit', function() {
+        expect(noProducers.profit).toEqual(0);
+    })
+})
